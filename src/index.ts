eb14c68b62b1f6a270d3ad49a528881822d63237
Christos Stylidis <christos@autotetra.com>
2025-10-16 23:42:13 +0300

    implementing auth

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
 import dotenv from "dotenv";
-dotenv.config();
+import express from "express";
 import { connectDB } from "./config/db";
+import authRoutes from "./routes/authRoutes";
+import cors from "cors";
 
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// Enable CORS
+app.use(cors());
+
+// Middleware to parse JSON
+app.use(express.json());
+
+app.get("/test", (req, res) => res.send("Server is running"));
+
+// Connect to MongoDB
 connectDB();
+
+// Use routes
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
